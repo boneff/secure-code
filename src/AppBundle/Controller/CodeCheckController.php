5bc52a28f0d70a1b19e34626812d251b727622c9
boneff <boneff.i@gmail.com>
2017-11-19 15:29:06 +0200

    Added CodeCheckController check action, minor template fixes

--- src/AppBundle/Controller/CodeCheckController.php
+++ src/AppBundle/Controller/CodeCheckController.php
@@ -3,9 +3,19 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\CodeCheck;
+use AppBundle\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * Codecheck controller.
@@ -30,6 +40,40 @@ class CodeCheckController extends Controller
 
         return $this->render('codecheck/index.html.twig', array(
             'codeChecks' => $codeChecks,
+        ));
+    }
+
+    /**
+     * Run code check on project
+     *
+     * @Route("/run/{$projectId}", name="codecheck_run")
+     * @Method("GET")
+     */
+    public function runAction(Request $request, KernelInterface $kernel)
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER', null, 'Unable to access this page!');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $application = new Application($kernel);
+        $project = $em->getRepository('AppBundle:Project')->find($request->get('projectId'));
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command'  => 'security:check',
+            'lockfile' => $this->getRemoteLockFile($project->getRepositoryUrl())
+        ]);
+
+        // You can use NullOutput() if you don't need the output
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        // return the output, don't use if you used NullOutput()
+        $content = $output->fetch();
+        // $codeCheck = new Codecheck();
+
+        return $this->render('codecheck/result.html.twig', array(
+            'checkResult' => $content,
         ));
     }
 
@@ -142,5 +186,37 @@ class CodeCheckController extends Controller
             ->setMethod('DELETE')
             ->getForm()
         ;
+    }
+
+    /**
+     * @param $url
+     * @return string
+     */
+    private function getRemoteLockFile($url)
+    {
+        $filePath = '';
+        try {
+            $file = file_get_contents($url
+                . DIRECTORY_SEPARATOR
+                . 'blob'
+                . DIRECTORY_SEPARATOR
+                . 'master'
+                . DIRECTORY_SEPARATOR
+                . 'composer.lock');
+        } catch (Exception $e) {
+
+        }
+
+        $fs = new Filesystem();
+        try {
+            $randomNum = mt_rand();
+            $fs->mkdir('/tmp/'.$randomNum);
+            $filePath = '/tmp/' . $randomNum . '/composer.lock';
+            $fs->dumpFile($filePath, $file);
+        } catch (IOException $e) {
+            echo "An error occurred while creating your directory at ".$e->getPath();
+        }
+
+        return $filePath;
     }
 }
